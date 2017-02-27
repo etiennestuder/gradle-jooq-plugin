ddcb2defb7256678e9f11b7743a7ca725a6fe1d7
Felipe Fernández <felipe@gradle.com>
2017-02-27 14:41:39 +0000

    Update happy path tests to include a list in the DSL.

--- src/test/groovy/nu/studer/gradle/jooq/JooqFuncTest.groovy
+++ src/test/groovy/nu/studer/gradle/jooq/JooqFuncTest.groovy
@@ -3,7 +3,6 @@ package nu.studer.gradle.jooq
 import groovy.sql.Sql
 import org.gradle.testkit.runner.TaskOutcome
 import spock.lang.AutoCleanup
-import spock.lang.Ignore
 import spock.lang.Shared
 
 import java.sql.DriverManager
@@ -50,25 +49,13 @@ class JooqFuncTest extends BaseFuncTest {
         result.task(':generateSampleJooqSchemaSource').outcome == TaskOutcome.UP_TO_DATE
     }
 
-     void "participates in the up-to-date checks when the configuration is different"() {
+    void "participates in the up-to-date checks when the configuration is different"() {
         given:
         buildFile << buildWithJooqPluginDSL()
         runWithArguments('build')
 
         buildFile.delete()
         buildFile << buildWithJooqPluginDSL("different.target.package.name")
-
-        when:
-        def result = runWithArguments('build')
-
-        then:
-        result.task(':generateSampleJooqSchemaSource').outcome == TaskOutcome.SUCCESS
-    }
-
-    @Ignore
-    void "successfully applies DSL with forced types"() {
-        given:
-        buildFile << buildWithForcedTypes()
 
         when:
         def result = runWithArguments('build')
@@ -113,61 +100,18 @@ jooq {
                name = 'org.jooq.util.h2.H2Database'
                includes = '.*'
                excludes = ''
-           }
-           generate {
-           }
-           target {
-               packageName = '$targetPackageName'
-           }
-       }
-   }
-}
-"""
-    }
-
-    private String buildWithForcedTypes() {
-        """
-plugins {
-    id 'nu.studer.jooq' version '2.0.2'
-}
-
-apply plugin: 'java'
-
-repositories {
-    jcenter()
-}
-
-dependencies {
-    compile 'org.jooq:jooq'
-    jooqRuntime 'com.h2database:h2:1.4.193'
-}
-
-jooq {
-   version = '3.9.0'
-   edition = 'OSS'
-   sample(sourceSets.main) {
-       jdbc {
-           driver = 'org.h2.Driver'
-           url = 'jdbc:h2:~/test;AUTO_SERVER=TRUE'
-           user = 'sa'
-           password = ''
-       }
-       generator {
-           name = 'org.jooq.util.DefaultGenerator'
-           strategy {
-               name = 'org.jooq.util.DefaultGeneratorStrategy'
-           }
-           database {
-               name = 'org.jooq.util.h2.H2Database'
-               includes = '.*'
-               excludes = ''
-               forcedTypes = [
-                   {
-                       userType = 'java.time.LocalDateTime'
-                       converter = 'com.company.dao.LocalDateTimeConverter'
-                       expression = '.*\\\\.DATE_TIME.*'
-                   }
-              ]
+               forcedTypes {
+                 forcedType {
+                     name = 'varchar'
+                     expression = '.*'
+                     types = 'JSONB?'
+                 }
+                 forcedType {
+                     name = 'varchar'
+                     expression = '.*'
+                     types = 'INET'
+                 }
+               }
            }
            generate {
            }
