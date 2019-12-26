6ef7b278f4242206ac1cfe518432ce1cb4fa3fcd
Etienne Studer <etienne@studer.nu>
2019-12-26 14:03:54 +0100

    Fix incremental build annotation usage where behavior in Gradle changed between 5.0 and 5.6

--- src/main/groovy/nu/studer/gradle/jooq/JooqTask.groovy
+++ src/main/groovy/nu/studer/gradle/jooq/JooqTask.groovy
@@ -54,8 +54,7 @@ class JooqTask extends DefaultTask {
     @Internal
     Configuration configuration
 
-    @Internal
-    Configuration normalizedConfiguration
+    private Configuration normalizedConfiguration
 
     @Input
     @SuppressWarnings("GroovyUnusedDeclaration")
