580d5e93a6cb9462f14b3005ae5006eb9c639fdd
davinkevin <davin.kevin@gmail.com>
2020-08-29 17:00:17 +0200

    doc(spring-boot): provide example using version defined in spring boot in jooq plugin
    Link to https://github.com/etiennestuder/gradle-jooq-plugin/issues/137

---
+++ example/spring-boot-use-spring-boot-jooq-version-in-plugin/src/main/java/nu/studer/gradle/jooq/example/springboot/SpringBootUseSpringBootJooqVersionInPluginApplication.java
@@ -0,0 +1,13 @@
+package nu.studer.gradle.jooq.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUseSpringBootJooqVersionInPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUseSpringBootJooqVersionInPluginApplication.class, args);
+    }
+
+}
