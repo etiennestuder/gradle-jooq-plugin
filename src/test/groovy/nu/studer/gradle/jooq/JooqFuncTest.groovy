96a189ba89c1b18493a7df8b2283d6576cf07ab3
Etienne Studer <etienne@studer.nu>
2021-06-20 22:59:37 +0200

    Use 6.9 to test config caching

--- src/test/groovy/nu/studer/gradle/jooq/JooqFuncTest.groovy
+++ src/test/groovy/nu/studer/gradle/jooq/JooqFuncTest.groovy
@@ -55,7 +55,7 @@ tasks.named('generateJooq').configure { outputs.cacheIf { true } }
 
     void "can invoke jOOQ task from configuration DSL with Gradle configuration cache enabled"() {
         given:
-        gradleVersion = GradleVersion.version('7.1')
+        gradleVersion = GradleVersion.version('6.9')
         buildFile << buildWithJooqPluginDSL()
 
         when:
