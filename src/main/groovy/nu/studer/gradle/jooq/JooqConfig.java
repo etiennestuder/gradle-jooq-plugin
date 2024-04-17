58ab15d204ebd8c380de7d9e49f20f8d43a69bc8
Nathan Strong <102557081+nstrong-scw@users.noreply.github.com>
2024-04-17 15:30:35 -0700

    add getter for name field

--- src/main/groovy/nu/studer/gradle/jooq/JooqConfig.java
+++ src/main/groovy/nu/studer/gradle/jooq/JooqConfig.java
@@ -61,6 +61,10 @@ public class JooqConfig {
         return generateSchemaSourceOnCompilation;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public Provider<Directory> getOutputDir() {
         return outputDir;
     }
